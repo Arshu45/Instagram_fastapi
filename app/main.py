1251ba05c309ae222bea62b24f163fca881e6d21
arsha45 <arsha@cybage.com>
2025-08-24 03:34:27 +0530

    testing render

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World testing incremental updates in render"}
 
 
 # Include routers
